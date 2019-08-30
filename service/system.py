d37140029ca1f75ea098cec758ca18b0a35c0fa5
user2684 <user2684@users.noreply.github.com>
2019-08-30 23:31:28 +0200

    Added support for the new sensor registration

--- service/system.py
+++ service/system.py
@@ -66,4 +66,9 @@ class System(Service):
             
     # What to do when receiving a new/updated configuration for this module
     def on_configuration(self,message):
-        pass
+        # register/unregister the sensor
+        if message.args.startswith("sensors/"):
+            if message.is_null: 
+                sensor_id = self.unregister_sensor(message)
+            else: 
+                sensor_id = self.register_sensor(message)
